605552c88abacb4f6635c789fb53d9f5799ed2f4
liriondev <liriondev>
2023-02-01 23:45:00 +0200

    Add install

---
+++ install.sh
@@ -0,0 +1,14 @@
+echo -e ' \e[103m +-+-+-+-+-+-+-+-+ +-+-+-+-+-+-+-+ \e[0m\n' \
+        '\e[103m |T|G|S|T|u|r|b|o| |i|n|s|t|a|l|l| \e[0m\n' \
+        '\e[103m +-+-+-+-+-+-+-+-+ +-+-+-+-+-+-+-+ \e[0m\n'
+
+echo 'Starting install...'
+sleep 1
+
+apt update 
+apt -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"  upgrade -y
+apt isntall python3 git -y
+pip3 install pipenv
+git clone https://github.com/liriondev/TGSTurbo
+cd TGSTurbo
+pipenv install
